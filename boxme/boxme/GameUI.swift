5cf94e56991a80dc6f821c3ef1cbcc28812ea514
Yuhan Liu <yliu1021@hotmail.com>
2016-07-27 10:35:10 -0400

    Added GameUI Changed MenuUI to import UIKit and inherit UIView

--- boxme/boxme/GameUI.swift
+++ boxme/boxme/GameUI.swift
@@ -1,5 +1,5 @@
 //
-//  MenuUI.swift
+//  GameUI.swift
 //  boxme
 //
 //  Created by Yuhan Liu on 7/27/16.
@@ -9,7 +9,7 @@
 import Foundation
 import UIKit
 
-class MenuUI: UIView {
+class GameUI: UIView {
     
     
     
